7edef0baf445d16468052db281e37178943516eb
Nguyen Duc Tung <Duc Tung>
2021-07-19 17:26:41 +0700

    create project

---
+++ src/main/java/com/ncovid/NcovidApplication.java
@@ -0,0 +1,13 @@
+package com.ncovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcovidApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NcovidApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ncovid/NcovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcovidApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
