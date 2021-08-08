69db73178712cff7132977caff8d907a8405de03
Nguyen Duc Tung <Duc Tung>
2021-08-08 11:49:05 +0700

    ipdate

--- src/main/java/com/ncovid/configration/MultithreadingConfig.java
+++ src/main/java/com/ncovid/configration/MultithreadingConfig.java
@@ -20,9 +20,9 @@ public class MultithreadingConfig {
   @Bean(name ="taskExecutor")
   public Executor taskExecutor(){
     ThreadPoolTaskExecutor executor= new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(20);
-    executor.setMaxPoolSize(50);
-    executor.setQueueCapacity(100);
+    executor.setCorePoolSize(2);
+    executor.setMaxPoolSize(5);
+    executor.setQueueCapacity(50);
     executor.setThreadNamePrefix("Thread-");
     executor.initialize();
     executor.shutdown();
